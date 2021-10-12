07c0ca25548c783104fcefe699bf5e47e7056972
RMSAJ <92260209+RMSAJ@users.noreply.github.com>
2021-10-12 18:24:07 +0300

    Rakan Salary co.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,37 +1,32 @@
 
 
 fun main(args: Array<String>) {
-    println(" welcome to Rakan calcolator ")
-    println(" pls write your inter calc operations")
+    println(" please write the annual salary")
+    var salary = readLine()!!
+    println(" pls input the kpi (1- Excellent 2- good 3- poor)")
+    var kpi = readLine()!!
 
-    var firstNumber = readLine()!!
-    println(" pls use only (+, -, *, /,%)")
-   var operation = readLine()
-    println("pls write ur second number")
-    var secondNumber = readLine()!!
-
-    if( operation == "+"){
-        println(firstNumber.toInt() + secondNumber.toInt())
+    if ( kpi.toInt() == 1 ){
+        var raise = (salary.toInt() * 0.06)
+        println(" Employee of the month!! you deserve a raise of: SAR ${raise} ")
+        println(" your Salary now: ")
+        println(raise + salary.toInt())
     }
-    else if (operation == "-"){
-       println( firstNumber.toInt() - secondNumber.toInt())
-
-
-
+    else if ( kpi.toInt() == 2 ){
+        var raise = (salary.toInt() * 0.04)
+        println(" do better next year!! you deserve a raise of: SAR ${raise} ")
+        println(" your Salary now: ")
+        println(raise + salary.toInt())
     }
-    else if ( operation == "/"){
-        println(firstNumber.toInt() / secondNumber.toInt() )
+    else if ( kpi.toInt() == 3 ) {
+        var raise = (salary.toInt() * 0.015)
+        println(" very bad employee!!! you deserve a raise of: SAR ${raise} ")
+        println(" your Salary now: ")
+        println(raise + salary.toInt())
     }
-    else if ( operation == " *"){
-        println(firstNumber.toInt() * secondNumber.toInt())
+    else {
+        println( " pls write the correct numbers")
     }
-    else if( operation == "%"){
-        println(firstNumber.toInt() % secondNumber.toInt())
-    }
-    else{
-        println("null")
-    }
-
     }
 
 
