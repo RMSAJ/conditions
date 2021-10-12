142b5b2facba13d604f3c88cebb14827c7836d12
RMSAJ <92260209+RMSAJ@users.noreply.github.com>
2021-10-12 16:45:22 +0300

    The leap Year

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,34 @@
 
 
 fun main(args: Array<String>) {
-    var appleNo = readLine()!!
-    var dozens = appleNo.toInt()/12 // number of dozes we have
-    var remainApple = appleNo.toInt() % 12 // the remider of the apples
-    println("number of dozens = "+ dozens)
-    println("apples remain = " + remainApple)
+    var years = readLine()!!
+    var year400 = years.toInt()%400// the reminder of years when devided by 400
+    var year100 = years.toInt()%100 // the reminder of years when devided by 100
+    var year4 = years.toInt() % 4 // the reminder of years when devided by 4
+    if( years < 1582.toString()){
+        if( year4 == 0 ){
+            println("it's a leap year")
+        }
+        else {
+            println("its normal year")
+        }
+    }
+    else if ( years >= 1582.toString()){
+        if( year400 == 0){
+            println("Leap Year")
+        }
+        else if ( year100 == 0){
+            println("normal year")
+        }
+        else if ( year4 == 0){
+            println(" leap year")
+        }
+        else {
+            println("normal year")
+        }
+
+    }
+
     // Try adding program arguments at Run/Debug configuration
     //  println("Program arguments: ${args.joinToString()}")
 }
