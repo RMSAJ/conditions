21c21cc263123d4dc000043a4f28e25af04bb63b
RMSAJ <92260209+RMSAJ@users.noreply.github.com>
2021-10-13 19:39:33 +0300

    the aura Assignment& TODO

---
+++ src/main/kotlin/the_Unreturn_function.kt
@@ -0,0 +1,17 @@
+/* in kotlin we use return tybes for our variavles, however there are some cases we want functions that deosn't
+return anything similar to (void) in kava which is the (unit) return type we use it only in some situsntions like when
+a player makes an input and we call the (unit) type function to display a message
+ */
+/* the another practial function that alot professional developers use which is the (todo function), its usage is
+ indicated by the developer in the name of the function to inform him that the function is not yet completed or need
+ to be rechecked
+ */
+// example below
+private fun myFavNums(){
+
+     TODO()    print(" I'm still thinking about what is my fav numbers")
+
+
+}
+
+
